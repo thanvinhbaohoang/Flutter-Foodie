1fd00eea82d3e44b1a5e105b44758bd601966f7a
Harold <harold@pop-os.localdomain>
2023-06-23 18:49:47 -0400

    SearchField styled

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -13,6 +13,56 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(),
+      body: Column(
+        children: [
+          _searchField(),
+        ],
+      ),
+    );
+  }
+
+  Container _searchField() {
+    return Container(
+      margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            spreadRadius: 0, //spread radius
+            blurRadius: 40, // blur radius
+          ),
+        ],
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: "Search Restaurants ...",
+          hintStyle: TextStyle(
+            color: Colors.black.withOpacity(0.2),
+            fontSize: 15,
+          ),
+          contentPadding: const EdgeInsets.all(15),
+          filled: true,
+          fillColor: Colors.white,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide.none,
+          ),
+          prefixIcon: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Icon(
+              Icons.search,
+              color: Colors.black.withOpacity(0.5),
+            ),
+          ),
+          suffixIcon: const Padding(
+            padding: EdgeInsets.all(15),
+            child: Icon(
+              Icons.settings,
+              color: Colors.grey,
+            ),
+          ),
+        ),
+      ),
     );
   }
 
